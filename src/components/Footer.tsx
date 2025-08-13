f2528319907f6bc65fc8e87522202b4954a2080c
jingjingbic <jingjingbic@gmail.com>
2025-08-13 14:27:06 -0700

    Reduce the footer height.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,14 +9,14 @@ import { useBasePath } from '../lib/contexts/ConfigContext'
 export function Footer() {
   const basePath = useBasePath()
   return (
-    <footer className="w-full py-4 bg-muted flex-grow-1">
+    <footer className="w-full py-2 bg-muted flex-grow-1">
       <div className="mx-auto max-w-7xl px-4 md:flex md:justify-between md:items-start text-sm">
         {/* Left Column: LEGAL */}
-        <div className="mb-6 md:mb-0">
-          <h4 className="scroll-m-20 text-lg font-semibold tracking-tight text-foreground">
+        <div className="mb-3 md:mb-0">
+          <h4 className="scroll-m-20 text-base font-semibold tracking-tight text-foreground">
             Legal
           </h4>
-          <p className="leading-7 [&:not(:first-child)]:mt-6 text-foreground">
+          <p className="leading-5 mt-2 text-foreground">
             NDEx{' '}
             <Link
               href="https://home.ndexbio.org/disclaimer-license/"
@@ -25,7 +25,7 @@ export function Footer() {
               License, Terms &amp; Privacy Policy
             </Link>
           </p>
-          <p className="mt-2 text-muted-foreground">
+          <p className="mt-1 text-muted-foreground text-xs">
             Copyright © 2013–
             {new Date().getFullYear()}, The Regents of the University of
             California, The Cytoscape Consortium. All rights reserved.
@@ -34,10 +34,10 @@ export function Footer() {
 
         {/* Right Column: FOLLOW US */}
         <div>
-          <h3 className="scroll-m-20 text-lg font-semibold tracking-tight text-foreground">
+          <h3 className="scroll-m-20 text-base font-semibold tracking-tight text-foreground">
             Follow Us
           </h3>
-          <div className="container py-2 flex items-center">
+          <div className="py-1 flex items-center">
             {/* YouTube Link */}
             <Link
               href="https://www.youtube.com/@ndexproject"
